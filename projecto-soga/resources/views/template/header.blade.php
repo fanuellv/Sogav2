b35b67a18004b0999b1af90325f4baefa4e4f418
fanuellv <fanueljuniorlv2@gmail.com>
2024-06-13 21:37:47 +0100

    criação do template

---
+++ projecto-soga/resources/views/template/header.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @yield('titulo')
+    <link rel="stylesheet" href="assets/css/header.css">
+
+</head>
+<body>
+    <div class=".principal">
+        <header class="top">
+            <img src="assets/img/soga.svg" alt="">
+
+            <div class="icon">
+                <a href="../feed" class="feed">
+                    <div  class="ifeed">
+
+                    </div>
+                </a>
+                <a href="../feed" class="friend">
+                    <div  class="ifriend">
+
+                    </div>
+                </a>
+                <a href="../feed" class="group">
+                    <div  class="igroup">
+
+                    </div>
+                </a>
+                <a href="../feed" class="chat">
+                    <div  class="ichat">
+
+                    </div>
+                </a>
+                
+            </div>
+            <div class="search">
+                <a href="">
+                    <div class="lupa"></div>
+                </a>
+                <input type="text" class="txtSearch" placeholder="pesquisa">
+            </div>
+
+        </header>
+        <div class="conteiner">
+            <div class="left">
+
+            </div>
+            <div class="center"
+            ></div>
+            <div class="right">
+
+            </div>
+        </div>
+    </div>
+</body>
+</html>
