3bef1d76968b669bfe296768540df5bf818dc509
fanuel <fanueljuniorlv2@gmail.com>
2024-06-18 20:30:42 +0100

    crud

---
+++ database/migrations/2024_06_18_085055_create_notificacaos_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('notificacaos', function (Blueprint $table) {
+            $table->id(); // Cria uma coluna 'id' do tipo bigint auto-incrementada
+            $table->string('texto');
+            $table->foreignId('usuario_id')->constrained('usuarios')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notificacaos');
+    }
+};
---
+++ database/migrations/2024_06_18_085710_create_curtidas_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('curtidas', function (Blueprint $table) {
+            $table->id(); // Cria uma coluna 'id' do tipo bigint auto-incrementada
+            $table->foreignId('usuario_id')->constrained('usuarios')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('curtidas');
+    }
+};
